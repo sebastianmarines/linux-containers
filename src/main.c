9203059b0f17b4b6c7b2b4ba49d81a104f4fd484
Sebastian Marines <sebastian0marines@gmail.com>
2022-01-02 19:16:10 -0600

    Start shell inside container

--- src/main.c
+++ src/main.c
@@ -4,13 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 int child();
 
 int main()
 {
-    int cloneFlags = CLONE_NEWUTS | CLONE_NEWPID;
-    pid_t container = clone(child, malloc(4096) + 4096, SIGCHLD | cloneFlags, NULL);
+    int clone_flags = CLONE_NEWUTS | CLONE_NEWPID | CLONE_NEWNS | CLONE_NEWNET | CLONE_NEWIPC;
+    pid_t container = clone(child, malloc(4096) + 4096, SIGCHLD | clone_flags, NULL);
     if (container == -1)
     {
         perror("clone");
@@ -23,8 +24,10 @@ int main()
 
 int child()
 {
-    int host = sethostname("container", 9);
-    printf("Hello from child! PID=%d\n", getpid());
-    execv("/bin/sh", NULL);
+    char *cmd[] = {"/bin/sh", NULL};
+    sethostname("container", 9);
+    chroot("alpine");
+    chdir("/");
+    execv("/bin/sh", cmd);
     return 0;
 }
