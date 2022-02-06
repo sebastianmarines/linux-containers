81efd568ccc229652708697481db99e4bbb1eb58
Sebastian Marines <sebastian0marines@gmail.com>
2022-02-05 22:49:05 -0600

    Create user and group mapping

--- src/main.c
+++ src/main.c
@@ -5,13 +5,47 @@
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <sys/mount.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/limits.h>
+#include <string.h>
 
 int child();
+
+static void
+update_map(char *mapping, char *map_file)
+{
+    int fd, j;
+    size_t map_len; /* Length of 'mapping' */
+
+    /* Replace commas in mapping string with newlines */
+
+    map_len = strlen(mapping);
+    for (j = 0; j < map_len; j++)
+        if (mapping[j] == ',')
+            mapping[j] = '\n';
+
+    fd = open(map_file, O_RDWR);
+    if (fd == -1)
+    {
+        fprintf(stderr, "open %s: %s\n", map_file, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (write(fd, mapping, map_len) != map_len)
+    {
+        fprintf(stderr, "write %s: %s\n", map_file, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
+}
 
 int main(int argc, char *argv[])
 {
     int child_args_len = argc < 2 ? 2 : argc;
     char *child_args[child_args_len];
+    char map_path[PATH_MAX];
     if (argc < 2)
     {
         child_args[0] = "/bin/bash";
@@ -32,7 +66,15 @@ int main(int argc, char *argv[])
         exit(1);
     }
     printf("Container pid: %d\n", container);
+    snprintf(map_path, PATH_MAX, "/proc/%ld/uid_map",
+             (long)container);
+    char *uid_map = "0 1000 1";
+    update_map(uid_map, map_path);
     waitpid(container, NULL, 0);
+    snprintf(map_path, PATH_MAX, "/proc/%ld/gid_map",
+             (long)container);
+    char *gid_map = "0 1000 1";
+    update_map(gid_map, map_path);
     return 0;
 }
 
